6f23456df276ce81bbb3f171d7d20a3a16fbfcc9
Kusnetsov Roman <KusnetsovRA@gmail.com>
2024-05-23 16:55:30 +0300

    Add markup all screen

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../main/main';
+
+type AppScreenProps = {
+  countPlace: number;
+}
+
+function App({countPlace}: AppScreenProps): JSX.Element {
+  return (
+    <Main countPlace={countPlace}/>
+  )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+
+const settings = {
+  COUNT_PLACE: 2,
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App countPlace={settings.COUNT_PLACE}/>
   </React.StrictMode>,
 );
